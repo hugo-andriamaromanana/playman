2252a08ac21e086beb0c4c88284ef58ff36e6d03
hugo.andriamaromanana@laplateforme.io <hugo.andriamaromanana@laplateforme.io>
2023-05-18 09:02:53 +0200

    [ADD] More infos/ testing git ignore

--- scrapper.py
+++ scrapper.py
@@ -38,22 +38,56 @@ def get_playlist_items(playlist_id):
         arr.append(response)
     return arr
 
+def more_metadata(url):
+    url = f'https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,topicDetails,statistics&id={url}&key={PARAMS["key"]}'
+    response = requests.get(url).json()
+    return {
+        'duration': response['items'][0]['contentDetails']['duration'],
+        'publishedAt': response['items'][0]['snippet']['publishedAt'],
+        'topic_categories': response['items'][0]["topicDetails"]['topicCategories'],
+        'view_count': response['items'][0]['statistics']['viewCount'],
+        'like_count': response['items'][0]['statistics']['likeCount'],
+        'comment_count': response['items'][0]['statistics']['commentCount'],
+        'tags': response['items'][0]['snippet']['tags'],
+        'channel_title': response['items'][0]['snippet']['channelTitle']
+    }
+
 def get_song_data(data,playlist_name,playlist_ID):
+
     count=0
+
     for i in range(len(data)):
         try:
+
             for j in range(len(data[i]['items'])):
+
+                url=data[i]['items'][j]['snippet']['resourceId']['videoId']
+
+                meta_data= more_metadata(url)
+
                 song = {}   
                 song['title'] = clean(data[i]['items'][j]['snippet']['title'])
-                song['publishedAt'] = data[i]['items'][j]['snippet']['publishedAt']
-                song['channel_ID'] = data[i]['items'][j]['snippet']['videoOwnerChannelId']
-                song['url'] = data[i]['items'][j]['snippet']['resourceId']['videoId']
-                song['playlist_ID'] = playlist_ID
+                song['publishedAt'] = meta_data['publishedAt']
+                song['duration'] = meta_data['duration']
+                song['topic_categories'] = meta_data['topic_categories']
+                song['view_count'] = meta_data['view_count']
+                song['like_count'] = meta_data['like_count']
+                song['comment_count'] = meta_data['comment_count']
+                song['tags'] = meta_data['tags']
+                song['channel_title'] = meta_data['channel_title']
                 song['playlist_name'] = playlist_name
+                song['channel_ID'] = data[i]['items'][j]['snippet']['videoOwnerChannelId']
+                song['playlist_ID'] = playlist_ID
+                song['url'] = url
+
                 if add_dic_to_items_csv(song):
                     count+=1
+
         except:
+
+            print(f'[ERROR]: {data[i]["items"][j]["snippet"]["title"]} not added to database')
             pass
+
     print(f'[SUCCESS]: {count} songs added to database from {playlist_name}')
 
 
@@ -68,4 +102,4 @@ def scrap_all_playlists_from():
         scrap_playlist(dic[i],i)
 
 # # scrap_playlist('PLn4GvABOzCQt4ciDfegKgW_Q6kDLfqFa-','Trash')
-scrap_all_playlists_from()
+# scrap_all_playlists_from()
